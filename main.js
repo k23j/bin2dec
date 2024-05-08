9061fe9ac5a16a15e1bb36de77beebe2a2abdcaf
k23j <passos.dev.k@gmail.com>
2024-05-08 14:52:42 -0300

    v1

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 
     const conversionType = document.getElementById("conversionType");
     const result = document.getElementById("result");
+    const input = document.getElementById("input");
   
 
     let bin2dec = true;
 
-    const regexBinary = "^[01]+$";
-    const regexDecimal = "^-?\d+$";
+    const regexBinary = /^[01]+$/;
+    const regexDecimal = /^-?\d+$/;
 
     document.getElementById("swapOp")
     .addEventListener('click', function(event){
@@ -15,19 +16,53 @@
         swap();
     });
 
-    // document.getElementById("convertButton")
-    // .addEventListener('click', function(event){
-    //     event.preventDefault();
-    //     convert();
-    // });
+    document.getElementById("convertButton")
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        tryConvert();
+    });
 
     function swap() {
-        bin2dec = !bin2dec;
-        updateConverionTypeText();
+      if (isValidInput) {
+        const userInput = getUserInput();
+        const convertedResult = convert(userInput);
+        
+        result.innerHTML = userInput;
+        input.value = convertedResult;
+      }
+
+      bin2dec = !bin2dec;
+      updateConverionTypeText();
     }
 
-    function convert() {
-        document.getElementById("form").submit();
+    function tryConvert() {
+      if (isValidInput) {
+        convert(input.value.trim());
+      }      
+    }
+
+    function getUserInput() {
+      return input.value.trim();
+    }
+
+    function isValidInput() {
+      const userInput = getUserInput();
+
+      return (bin2dec ? regexBinary.test(userInput) : regexDecimal.test(userInput));
+    }
+    
+    function convert(input) {
+      if (bin2dec) {
+        let numResult = parseInt(input,2);
+        result.innerHTML = numResult.toString();
+
+        return numResult.toString();
+      } else {
+        let stringResult = Number(input).toString(2);
+        result.innerHTML = stringResult;
+
+        return stringResult;
+      }
     }
 
     function updateConverionTypeText() {
